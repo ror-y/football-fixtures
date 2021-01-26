0f289e1bd513d6e1ada1948dfc7bd61d3fc3cbe9
Rory Smith <rorysmith@Rorys-MacBook-Pro.local>
2021-01-26 23:18:33 +0000

    Initial commit

---
+++ main.node.js
@@ -0,0 +1,98 @@
+const unirest = require("unirest");
+const chalk = require("chalk");
+const log = console.log;
+const dateFormat = require("dateformat");
+var figlet = require("figlet");
+
+require("dotenv").config();
+
+const now = new Date();
+
+now.setDate(now.getDate() + 2);
+
+const date = dateFormat(now, "yyyymmdd");
+
+const req = unirest(
+  "GET",
+  "https://livescore6.p.rapidapi.com/matches/v2/list-by-date"
+);
+
+req.query({
+  Category: "soccer",
+  Date: date,
+});
+
+req.headers({
+  "x-rapidapi-key": process.env.API_KEY,
+  "x-rapidapi-host": "livescore6.p.rapidapi.com",
+  useQueryString: true,
+});
+
+const getTime = (timeStr) => {
+  const date = timeStr.toString().substring(0, 8);
+  const time = timeStr.toString().substring(8, 12);
+
+  const hour = time.substring(0, 2);
+  const min = time.substring(2, 4);
+  const hourNum = Number(hour);
+  const computedHour = hourNum + 17;
+  return `${computedHour}.${min}`;
+};
+
+req.end(function (res) {
+  if (res.error) throw new Error(res.error);
+
+  const premierLeagueObject = res.body.Stages.find(
+    (stage) => stage.Scd === "premier-league"
+  );
+
+  const matches = premierLeagueObject.Events;
+
+  log(
+    figlet.textSync("PL Games", {
+      font: "colossal",
+      horizontalLayout: "default",
+      verticalLayout: "default",
+      width: 80,
+      whitespaceBreak: true,
+    })
+  );
+
+  log("\n");
+
+  log("⚽️ Today's Premier League Fixures ⚽️");
+  log("\n");
+
+  matches.forEach((match) => {
+    const home = match.T1[0];
+    const away = match.T2[0];
+
+    const time = getTime(match.Esd);
+
+    const [homePrimaryColour, homeSecondaryColour] = [
+      home.Shrt.StC,
+      home.Shrt.Bs,
+    ];
+    const [awayPrimaryColour, awaySecondaryColour] = [
+      away.Shrt.StC,
+      away.Shrt.Bs,
+    ];
+
+    const blob = "█";
+
+    const [homeBlob1, homeBlob2] = [
+      chalk.hex(`#${homePrimaryColour}`)(blob),
+      chalk.hex(`#${homeSecondaryColour}`)(blob),
+    ];
+    const [awayBlob1, awayBlob2] = [
+      chalk.hex(`#${awayPrimaryColour}`)(blob),
+      chalk.hex(`#${awaySecondaryColour}`)(blob),
+    ];
+
+    log(
+      `${time}   ${homeBlob1} ${homeBlob2} ${home.Nm} vs ${away.Nm} ${awayBlob1} ${awayBlob2}`
+    );
+
+    log("\n");
+  });
+});
